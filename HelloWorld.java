dcd786417da90d50002dac06356a24e4fe73a094
Nikhil Kumar <40027722+nikhilkumar9499@users.noreply.github.com>
2018-06-13 14:32:29 +0530

    With Comments HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
-
+// Name of class: Main
 public class Main{
+  //Name of method: main
   public static void main(String...args){
+    //className: System, objectName: out, functionName: println
     System.out.println("Hello World!"); 
   }
 }
